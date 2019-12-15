a50a30416a03811c71e423cfc512c0e23370dafe
Alexms95 <silva.alexmatos@gmail.com>
2019-12-15 19:23:22 -0300

    Porcentagem de upload das fotos

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -5,6 +5,8 @@ import { PhotoService } from '../photo/photo.service';
 import { Router } from '@angular/router';
 import { AlertService } from 'src/app/shared/components/alert/alert.service';
 import { UserService } from 'src/app/core/user/user.service';
+import { HttpEvent, HttpEventType } from '@angular/common/http';
+import { finalize } from 'rxjs/operators';
 
 @Component({
 	selector: 'ap-photo-form',
@@ -16,6 +18,7 @@ export class PhotoFormComponent implements OnInit {
 	photoForm: FormGroup;
 	file: File;
 	preview: string;
+	percentDone = 0;
 
 	constructor(
 		private formBuilder: FormBuilder,
@@ -38,10 +41,21 @@ export class PhotoFormComponent implements OnInit {
 		const allowComments = this.photoForm.get('allowComments').value;
 		this.photoService
 			.upload(description, allowComments, this.file)
-			.subscribe(() => {
-				this.alertService.success('Upload complete!', true)
+			.pipe(finalize(() => {
 				this.router.navigate(['/user', this.userService.getUserName()])
-			})
+			}))
+			.subscribe((event: HttpEvent<any>) => {
+				if (event.type == HttpEventType.UploadProgress) {
+					this.percentDone = Math.round(100 * event.loaded / event.total)
+				} else if (event.type == HttpEventType.Response) {
+					this.alertService.success('Upload complete!', true)
+				}
+			},
+			err => {
+				console.log(err);
+				this.alertService.danger('Upload error!');
+			}
+			)
 	}
 
 	handleFile(file: File) {
